da54266c01c3ab67185c1872f4f1b849a937c6ca
praneethsaiii <praneethsai96@gmail.com>
2016-12-28 16:48:26 +0530

    sai praneeth mudivedu
    This is my personal website made by using materialize css

---
+++ sai praneeth mudivedu/sai praneeth mudivedu/index.php
@@ -0,0 +1,93 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="bootstrap/css/styles.css" rel="stylesheet" />
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css" media="screen,projection" />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+</head>
+<title>SAI PRANEETH MUDIVEDU</title>
+<?php
+
+
+
+?>
+<body>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col s7 m5">
+
+                <div class="card-panel teal">
+                    <div class="card ">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="sai.jpg" />
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">
+                                M SAI PRANEETH
+                                <i class="material-icons right">more_vert</i>
+                            </span>
+
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">
+                                DETAILS
+                                <i class="material-icons right">close</i>
+                            </span>
+                            <p>
+                                M SAI PRANEETH
+                                <br />
+                                M.TECH Software Engineering (Integrated)<br/>
+                                VIT UNIVERSITY
+                                <br />
+                                VELLORE
+                                <br />
+                                TAMILNADU
+                                <br />
+                                INDIA
+                                <br />
+                                P.NO-9524433019
+                                <br />
+                                EMAIL-praneethsai96@gmail.com
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            <div class="col s4 offset-s1"><p><h4>
+                WANNA HIRE ME?</h4></p>
+                <a href="RESUMEMSP.pdf" target="_blank"><h5>RESUME</h5></a>
+            </div>
+            </div>
+        </div>
+    <div class="row">
+        
+            <div class="col s4 offset-s2">
+            <ul>
+                <li>
+                    <a href="https://in.linkedin.com/in/mudivedu-sai-praneeth-5ba1537b" target="_blank"><img src="linkedin.png" width="60" height="60"/></a>
+                    <a href="https://github.com/praneethsaiii" target="_blank">
+                        <img src="git.png" width="70" height="70" />
+                    </a>
+                    <a href="https://www.facebook.com/sai.praneeth.9999" target="_blank">
+                        <img src="fb.png" width="70" height="70" />
+                    </a>
+                  
+                </li>
+            </ul>
+       
+    </div>
+   </div>
+    </body>
+</html>
